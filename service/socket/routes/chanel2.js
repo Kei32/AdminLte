59f310a1825bbe9ffd3a68a91b6ef7fd111ed4de
denis.taras <denis.taras@billing.ru>
2017-06-07 16:19:02 +0300

    Add NeDB and test use

--- service/socket/routes/chanel2.js
+++ service/socket/routes/chanel2.js
@@ -1,5 +1,4 @@
 module.exports = function (io,route) {
-
 	io.of(route).on('connection', function (socket) {
 	  socket.emit('message', { message: 'welcome to the chat2' });
 	  socket.on('send', function (data) {
@@ -7,5 +6,4 @@ module.exports = function (io,route) {
 	    io.of('/chanel_1').emit('message', data);
 	  });
 	});
-
 };
