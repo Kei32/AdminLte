e6829e1b8a4380edb21678a46f435db267cd6c03
denis.taras <denis.taras@billing.ru>
2017-08-01 14:51:51 +0300

    Add settings, sessions, users data store Add settings service

---
+++ service/settings.service.js
@@ -0,0 +1,17 @@
+var Datastore = require('nedb');
+var settingsService = {};
+
+//settingsService.getProperty
+//settingsService.setProperty
+//settingsService.removeProperty
+
+// settings = {
+//   name
+//   comment
+//   value
+// }
+
+
+//db.messages = new Datastore({filename : './db/messages.db', autoload : true});
+
+module.exports = settingsService;
